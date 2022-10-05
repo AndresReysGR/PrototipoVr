7b1eaf2dbee868454e8fe7ae13c56da94466485a
Alumno Ulsa <alumno@usn.edu.mx>
2022-10-05 15:04:57 -0700

    Upload1

---
+++ Coin.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Coin : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject puntaje;
+    public int puntos = 0;
+
+    public AudioSource audioSource { get { return GetComponent<AudioSource>(); } }
+    public AudioClip clip;
+
+    void Start()
+    {
+        gameObject.AddComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //metodo que usara leer el score
+    
+    public void OnEnter()
+    {
+        Destroy(this);
+        audioSource.PlayOneShot(clip);
+        puntos += 1;
+        puntaje.GetComponent<Text>().text = puntos.ToString();
+    }
+}
---
+++ EventListener.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventListener : MonoBehaviour
+{
+    private Renderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = gameObject.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnEnter()
+    {
+        renderer.material.color = Color.red;
+    }
+    public void OnEnxit()
+    {
+        renderer.material.color = Color.white;
+    }
+}
